00d2072f311710978ef79a00467b69e078032a73
Kim Singh <65624616+theKimSingh@users.noreply.github.com>
2024-01-23 17:42:24 -0800

    added name

--- app/names.js
+++ app/names.js
@@ -11,8 +11,9 @@ const names = [
     "Eun Be Cha",
     "Ira",
     "Arkita Jain",
-    "Donald Trump"
+    "Donald Trump",
     // TODO: add more here!
+    "Kim Singh"
 ];
 
 export default names;
