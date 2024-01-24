6242f8f44e1811b6e54ebc4418a0b2eb1574fe8e
Rboan08 <rdoan08@uw.edu>
2024-01-23 17:53:52 -0800

    Added my own name (Richie Doan)

--- app/names.js
+++ app/names.js
@@ -11,8 +11,8 @@ const names = [
     "Eun Be Cha",
     "Ira",
     "Arkita Jain",
-    "Donald Trump"
-    // TODO: add more here!
+    "Donald Trump",
+    "Richie Doan"
 ];
 
 export default names;
