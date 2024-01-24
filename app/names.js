6f9f10a61a345f6bac60bcc32ca35cf3355bfabd
RedPandaGaminq <timothyzhong2@gmail.com>
2024-01-23 17:54:19 -0800

    Added name

--- app/names.js
+++ app/names.js
@@ -1,18 +1,18 @@
 const names = [
-    "Eric Xiao",
-    "Juliette Park",
-    "Bill Yu",
-    "Nancy Le",
-    "Liza Nadtochiy",
-    "Grace Dao",
-    "Peter Corroon",
-    "Kira Brodsky",
-    "Katrina Ngor",
-    "Eun Be Cha",
-    "Ira",
-    "Arkita Jain",
-    "Donald Trump"
-    // TODO: add more here!
+  "Eric Xiao",
+  "Juliette Park",
+  "Bill Yu",
+  "Nancy Le",
+  "Liza Nadtochiy",
+  "Grace Dao",
+  "Peter Corroon",
+  "Kira Brodsky",
+  "Katrina Ngor",
+  "Eun Be Cha",
+  "Ira",
+  "Arkita Jain",
+  "Donald Trump",
+  "Tim",
 ];
 
 export default names;
